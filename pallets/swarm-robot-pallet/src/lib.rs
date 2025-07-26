374dac286293f5c24bf2bbdc89aaa01d2465c2a1
Damon Marc Rocha II <dmarcr1997@gmail.com>
2025-07-25 20:05:25 -0500

    Swarm robot Pallet config creation

--- pallets/swarm-robot-pallet/src/lib.rs
+++ pallets/swarm-robot-pallet/src/lib.rs
@@ -1,14 +1,35 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+#![cfg_attr(not(feature = "std" ), no_std)] 
+pub use pallet :: * ; 
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[frame::pallet] 
+pub mod pallet { 
+    use super :: * ; 
+    use frame :: prelude :: * ; 
+    
+    #[pallet::pallet] 
+    pub struct Pallet < T > ( _ ); 
+    
+    #[pallet::config] 
+    pub trait Config : frame_system :: Config { 
+        type RuntimeEvent : From < Event < Self >> + IsType << Self as frame_system :: Config > :: RuntimeEvent >;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        type RuntimeCall: IsType<<Self as frame_system::Config>::RuntimeCall> + From<Call<Self>>; 
+
+        type WeightInfo: WeightInfo;
+
+        #[pallet::constant]
+        type LocationId: Parameter + Member + Default + Copy + Has + MaxEncodedLen + TypeInfo;
+
+        type RobotId: Parameter + Member + Default + Copy + MaxEncodedLen + TypeInfo;
+
+        #[pallet::constant]
+        type MaxSwarmSize: Get<u32>;
+
+        #[pallet::constant]
+        type MaxLocations: Get<u32>;
+        type TaskDefinition: Parameter + Member + TypeInfo;
+
+        #[pallet::constant]
+        type MaxCommandWaitBlocks: Get<BlockNumberFor<Self>>;
     }
 }
